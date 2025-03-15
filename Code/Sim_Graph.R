daf40acb0d9fe6e015a90c457dea75f4f7a6abf5
antoniojustin <113738384+antoniojustin@users.noreply.github.com>
2025-03-15 17:01:10 -0400

    Clean

--- Code/Sim_Graph.R
+++
@@ -1,4 +0,0 @@
-source('/Users/antonioqin/Desktop/PhD Work/2.Mortality/2.Paper/Simulation Codes/Codes/Main_Fun.R')
-R1 = Simulation(B = 1, patients_num = 1000, hosp_coef_1 = 1.6, hosp_coef_2 = -2, tp1 = 0.2, tp2 = 0.3, tp3 = 0.4, tp4 = 0.5, tp5 = 0.6 )
-
-for i in 
